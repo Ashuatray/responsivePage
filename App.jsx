b5be39279c726c268ffa75dc90b4b728ef361579
Ashuatray <105809332+Ashuatray@users.noreply.github.com>
2024-06-12 18:33:48 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import Header from "./component/Header"
+import Body from "./component/Body"
+import Footer from "./component/Footer"
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="bg-color bg-amber-100 px-6 py-3 min-h-screen flex flex-col gap-10 md:px-10 md:py-7 md: justify-between" >
+        <Header/>
+        <Body/>
+        <Footer/>
+      </div>
+    </>
+  )
+}
+
+export default App
+//Ashutosh sharma 
